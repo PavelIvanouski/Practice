e4a59e94ba83914f1acbf29f2c659a4b28d6a810
Pavel Ivanouski <pavel.ivanouski@gmail.com>
2024-11-14 21:55:27 +0300

    practice 18

---
+++ src/by/it_academy/practice/practice18/lambda/MathOperation.java
@@ -0,0 +1,7 @@
+package by.it_academy.practice.practice18.lambda;
+
+@FunctionalInterface
+interface MathOperation {
+    int operate(int a, int b);
+
+}
---
+++ src/by/it_academy/practice/practice18/methodRefecence/Message.java
@@ -0,0 +1,12 @@
+package by.it_academy.practice.practice18.methodRefecence;
+
+public class Message {
+
+    public static void printMessage(String message) {
+        System.out.println("Message: " + message);
+    }
+
+    public String getMessage() {
+        return "Another message";
+    }
+}
